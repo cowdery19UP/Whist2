04b5b2ec9adc88ce2f0b3a87f434e0135bd2a6ee
PatrickMaloney <maloney20@up.edu>
2017-11-07 18:24:19 -0800

    Added WhistMainActivity

---
+++ app/src/main/java/edu/up/cs301/Whist/WhistMainActivity.java
@@ -0,0 +1,48 @@
+package edu.up.cs301.Whist;
+
+import java.util.ArrayList;
+
+import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.LocalGame;
+import edu.up.cs301.game.config.GameConfig;
+import edu.up.cs301.game.config.GamePlayerType;
+
+/**
+ * Created by PatrickMaloney on 11/7/17.
+ */
+
+public class WhistMainActivity extends GameMainActivity {
+
+    public static final int PORT_NUMBER = 2278;
+
+    @Override
+    public GameConfig createDefaultConfig(){
+        // Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        // Pig has two player types:  human and computer
+        playerTypes.add(new GamePlayerType("Local Human Player") {
+            public GamePlayer createPlayer(String name) {
+                return new WhistHumanPlayer(name);
+            }});
+        playerTypes.add(new GamePlayerType("Computer Player") {
+            public GamePlayer createPlayer(String name) {
+                return new WhistComputerPlayer(name);
+            }});
+
+        // Create a game configuration class for Pig:
+        GameConfig defaultConfig = new GameConfig(playerTypes, 1, 2, "Pig", PORT_NUMBER);
+        defaultConfig.addPlayer("Human", 0); // player 1: a human player
+        defaultConfig.addPlayer("Computer", 1); // player 2: a computer player
+        defaultConfig.setRemoteData("Remote Human Player", "", 0);
+
+        return defaultConfig;
+
+    }
+
+    @Override
+    public LocalGame createLocalGame() {
+        return new WhistLocalGame();
+    }
+}
